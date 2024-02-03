26f3ecf19b3c16f5d206d168c5c1fd127897b3fa
coderNiKhIlbro <mahajannikhil001@gmail.com>
2024-02-03 18:46:43 +0530

    initial commit

---
+++ app/src/main/java/com/example/whatsappclone/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.whatsappclone;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
